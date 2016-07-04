9b6d468101ce7a0cde16b2fa597673a23b15de01
Tomasz Polański <polanski.tomek@gmail.com>
2016-07-04 21:11:54 +0200

    Adding more examples

--- src/Intermediate.clj
+++ src/Intermediate.clj
@@ -1,5 +1,8 @@
  (ns Intermediate)
 
+; Documentation
+ (doc map)
+
 ; Let
 (def x 0)
 (let [x (inc x)] x)
@@ -24,3 +27,36 @@
   {:name (clojure.string/replace (:name test) #"^test" "")
    :line-count (:line-count test)})
 (test-rename {:name "testGetProperties" :line-count 25})
+
+; % as function
+(#(% [1 2 3]) count)
+
+; You can map (almost) everything
+(map #(str "This and " %) '(1 2 3))
+(map #(str "This and " (second %)) {:f "Value"})
+
+(map str ["1" "2" ] ["a" "b"])
+
+; List of functions
+(def sum #(reduce + %))
+(sum [1 2 3])
+(def avg #(/ (sum %) (count %)))
+(avg [2 6])
+
+(defn stats
+  [numbers]
+  (map #(% numbers) [sum count avg]))
+(stats [1 2 3 4])
+
+; Keys as functions
+(def some-list
+  [{:name "John" :last "Smith"}
+   {:name "Tomek" :last "Polanski"}])
+(map :name some-list)
+
+; New item in map
+(assoc {:key "value"} :newKey "newValue")
+(reduce (fn [new-map [key value]]
+          (assoc new-map key (inc value)))
+        {}
+        {:first 1 :second 2})
